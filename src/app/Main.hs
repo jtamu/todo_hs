97fab5144890fb5ad610e8777411bf163ab62510
jtamu <gtamu.dip@gmail.com>
2024-12-17 00:08:38 +0900

    feat: タスクの一覧表示

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -11,5 +11,18 @@
 
 module Main where
 
+import Data.List.Split
+import System.IO
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  withFile "tasks.csv" ReadMode $ \handle -> do
+    contents <- hGetContents handle
+    putStrLn "contents: "
+    let zipped = zip [1 ..] (tail $ lines contents) :: [(Integer, String)]
+        contents' = map formatLine zipped
+
+    mapM_ putStrLn contents'
+
+formatLine :: (Integer, String) -> String
+formatLine (i, line) = let [a, b] = splitOn "," line in show i ++ ") " ++ "task: " ++ a ++ ", status: " ++ b
